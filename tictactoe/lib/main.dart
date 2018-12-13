48d40687f33585902f141d18c105cc935df3b30e
quentinlao <quentinlao@outlook.com>
2018-12-13 14:00:43 +0100

    Trying with button and pressed

--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tic Tac Toe',
-
       home: MyHomePage(title: 'Tic Tac Toe'),
     );
   }
@@ -17,12 +16,15 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String status = "_";
+  String player = "Joueur X";
+  int turn = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,37 +35,72 @@ class _MyHomePageState extends State<MyHomePage> {
       new Container(
         child:
         new Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              new Text(
+                    player,
+                    style: new TextStyle(fontSize:12.0,
+                    color: const Color(0xFF000000),
+                    fontWeight: FontWeight.w200,
+                    fontFamily: "Roboto"),
+              ),
               new Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: () {
+                          setState(() {
+                            if (turn == 0) {
+                              player = "Tour du joueur X";
+                              turn = 1;
+                            } else {
+                              player = "Tour du joueur O";
+                              turn = 0;
+                            }
+                          });
+                        },
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
                     ),
 
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                    ),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                    )
+
+                  ]
+
+              ),
+              new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
                     ),
 
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                    ),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
                     )
                   ]
 
@@ -73,59 +110,21 @@ class _MyHomePageState extends State<MyHomePage> {
                   mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
                     ),
 
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
                     ),
-
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
-                    )
-                  ]
-
-              ),
-              new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
-                    ),
-
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
-                    ),
-
-                    new Text(
-                      "qWerty1",
-                      style: new TextStyle(fontSize:12.0,
-                          color: const Color(0xFF000000),
-                          fontWeight: FontWeight.w200,
-                          fontFamily: "Roboto"),
+                    new RaisedButton(
+                        child: Text(status),
+                        onPressed: null,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
                     )
                   ]
 
